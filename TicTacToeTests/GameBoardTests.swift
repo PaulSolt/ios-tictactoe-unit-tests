34cebb6d0beee78afe9a8da642c5441f65ff0061
Paul Solt <PaulSolt@gmail.com>
2019-09-11 13:02:58 -0400

    Added a unit test to very board is empty

--- TicTacToeTests/GameBoardTests.swift
+++ TicTacToeTests/GameBoardTests.swift
@@ -8,16 +8,54 @@
 
 import Foundation
 import XCTest
-
+@testable import TicTacToe
 
 
 class GameBoardTests: XCTestCase {
     
     // Unit Tests always start with "test"
     func testGameBoard() {
-        
-        XCTFail("The game board doesn't work properly")
+
+        // A compilation error is a unit test failure
+        let board = GameBoard()
+//         XCTFail("The game board doesn't work properly")
     }
+    
+    // test<What we are testing><What we expect>()
+    func testNewBoardIsEmpty() {
+        
+        let board = GameBoard()
+        
+//        board[(0, 0)] // Top left corner
+        
+        // How do we know if it's empty?
+        XCTAssertNil(board[(0, 0)])
+        
+        // (x, y)
+        // (0, 0), (1, 0), (2, 0)
+        // (0, 1), (1, 1), (2, 1)
+        // (0, 2), (1, 2), (2, 2)
+        
+        for x in 0 ..< 3 {
+            for y in 0 ..< 3 {
+                XCTAssertNil(board[(x, y)])
+            }
+        }
+    }
+    
+    
+    
+    
+    
+
+    // DON"T do this, documentation and code get out of sync
+//    // Tests that we can reset the game and the game board is reset
+//    func testA() {
+//
+//    }
+    
+    
+    
     
     func helper() {
         print("help manage the code...")
