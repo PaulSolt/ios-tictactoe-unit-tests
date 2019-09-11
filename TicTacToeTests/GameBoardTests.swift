415a6efd2de5f3707266882bd24996ff143e42ee
Paul Solt <PaulSolt@gmail.com>
2019-09-11 12:43:19 -0400

    Added a unit test target

---
+++ TicTacToeTests/GameBoardTests.swift
@@ -0,0 +1,27 @@
+//
+//  GameBoardTests.swift
+//  TicTacToeTests
+//
+//  Created by Paul Solt on 9/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+
+
+class GameBoardTests: XCTestCase {
+    
+    // Unit Tests always start with "test"
+    func testGameBoard() {
+        
+        XCTFail("The game board doesn't work properly")
+    }
+    
+    func helper() {
+        print("help manage the code...")
+    }
+    
+}
+
